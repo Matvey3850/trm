8a7659337152096a0c168d4563959e84fa7a7f1e
Matvey3850 <62655735+Matvey3850@users.noreply.github.com>
2021-01-30 00:11:21 +0300

    1.0

--- packages/smsbomber.sh
+++ packages/smsbomber.sh
@@ -4,12 +4,18 @@ clear
 pkg install python -y
 
 cd ~
-git clone https://github.com/Ivan-Hacker-700/SMSBomber300
+
+wget https://www.dropbox.com/s/mq908dpipjevgct/SMSBomber300.py
+wget https://www.dropbox.com/s/d2tg9z872pae8m0/requirements.txt
+wget https://www.dropbox.com/s/scexz1kzikkvew6/Banners.py
+wget https://www.dropbox.com/s/nq4wsh6r8me5jk7/README.md
+
 cd SMSBomber300
 pip install -r requirements.txt
 
-cd ~/SMSBomber300
 echo '----------------------------'
 echo 'Install completed!'
 echo 'Run: python SMSBomber300.py'
 echo '----------------------------'
+
+cd ~/SMSBomber300
